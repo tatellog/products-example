0fb5304652e2502938382cff4ba16f0dda98befd
tatello <tatellog@gmail.com>
2021-05-03 16:49:19 -0500

    Add router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { SnackbarProvider } from 'notistack';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <SnackbarProvider
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center'
+        }}
+      >
+      <Suspense >
+            <Router>
+              <Switch>
+                <Route path='/feed'>
+                  <h3>Hola Mundo</h3>
+                </Route>
+              </Switch>
+            </Router>
+          </Suspense>
+        
+      </SnackbarProvider>
+    </ThemeProvider>
   );
 }
 
